b0823b58eeca27bb83d76ba80e7e775f993455a4
BhavyaBibi <bhavyabibi007@gmail.com>
2022-08-25 14:14:25 +0400

    django code is added in the section

---
+++ py_client/basic.py
@@ -0,0 +1,14 @@
+from sys import api_version
+import requests
+
+endpoint ="https://github.com"
+endpoint="http://httpbin.org/status/200"
+
+endpoint="http://httpbin.org/anything"
+
+#phone -get-->camera -->App -->API -->CAMERA
+#Rest api -->web api
+
+
+
+# rest api as web based api_version
